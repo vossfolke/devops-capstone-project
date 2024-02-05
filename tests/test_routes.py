88900fc8602cd8059d38302d20506675cb449bf0
vossfolke <129680876+vossfolke@users.noreply.github.com>
2024-02-05 14:24:48 +0100

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -36,7 +36,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
@@ -187,4 +187,8 @@ class TestAccountService(TestCase):
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
-
+    def test_cors_security(self):
+        """This function should only return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
